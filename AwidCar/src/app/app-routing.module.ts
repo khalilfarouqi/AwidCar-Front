c0f6c532d936b95ad7f02a15ecaec3e653da0d23
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-07-17 17:27:41 +0100

    Generate component

--- AwidCar/src/app/app-routing.module.ts
+++ AwidCar/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component'
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "", component: SidebarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AwidCar/src/app/app.module.ts
+++ AwidCar/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule  } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    FooterComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
