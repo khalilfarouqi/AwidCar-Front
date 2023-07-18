e3bac66150921c79c48a1b652401fee00b4cf835
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-07-18 17:56:01 +0100

    Add car component

--- AwidCar/src/app/car/car.component.ts
+++ AwidCar/src/app/car/car.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarService } from '../service/car.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.scss']
 })
-export class CarComponent {
+export class CarComponent implements OnInit {
+
+  cars: any = [];
+  router: any;
+
+  constructor(private carService: CarService) { }
+
+  ngOnInit(): void {
+    this.getCar();
+  }
+
+  public getCar(){
+    this.carService.getAllCar()
+    .subscribe((result) => {
+      this.cars = result;
+      console.log(result)
+    })
+  }
+
+  public addNewCar(){
+    this.carService.addCar(this.cars).subscribe((item) => {
+      this.cars = [item,...this.cars]
+      this.router.navigate(["/car"])
+    })
+  }
 
 }
