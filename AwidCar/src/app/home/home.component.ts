1086fbfd07cc2afc58e68a9102a09faef9acd801
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-07-18 17:56:52 +0100

    Add new feature in home component

--- AwidCar/src/app/home/home.component.ts
+++ AwidCar/src/app/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../service/client.service';
+import { OrderService } from '../service/order.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  clients: any = [];
+  cars: any = [];
+  orders: any = [];
+
+  constructor(
+    private orderService: OrderService,
+    private clientService: ClientService) { }
+
+  ngOnInit(): void {
+    //this.getClient();
+    this.getOrder();
+  }
+
+  public getClient(){
+    this.clientService.getAllClient()
+    .subscribe((result) => {
+      this.clients = result;
+      console.log(result)
+    })
+  }
+
+  public getOrder(){
+    this.orderService.getAllOrder()
+    .subscribe((result) => {
+      this.orders = result;
+      console.log(result)
+    })
+  }
 }
