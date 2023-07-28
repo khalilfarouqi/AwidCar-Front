2ab7a34cbd390a3de676f9fb498bc88608e6bb9e
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-07-28 18:01:56 +0100

    Add http in service

--- AwidCar/src/app/service/car.service.ts
+++ AwidCar/src/app/service/car.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Car } from '../models/Car';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,26 @@ export class CarService {
   private apiUrl = 'http://localhost:8090/v1/car';
 
   constructor(private http: HttpClient) { }
-  
-  getAllCar() {
-    return this.http.get(`${this.apiUrl}/getAll`);
+
+  getAllCar(): Observable<Car> {
+    return this.http.get<Car>(`${this.apiUrl}/getAll`);
   }
 
-  addCar(car :any){
-    return this.http.post('${this.apiUrl}',car);
+  getCarById(id: number): Observable<Car> {
+    return this.http.get<Car>(`${this.apiUrl}/` + id);
   }
+
+  deleteCar(id: number){
+    return this.http.delete<Car>(`${this.apiUrl}/` + id);
+  }
+
+  addCar(car :Car){
+    return this.http.post(`${this.apiUrl}`,car);
+  }
+
+  postData(data: Car): Observable<Car> {
+    // Send POST request to the server
+    return this.http.post<Car>(this.apiUrl, data);
+  }
+
 }
