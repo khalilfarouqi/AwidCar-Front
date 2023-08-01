68d9d7d0f654ce179cabd6eb8dbd9d555f5de407
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-08-01 13:50:12 +0100

    Show all orders and to switch to details

--- AwidCar/src/app/order/order.component.ts
+++ AwidCar/src/app/order/order.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../service/order.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  orders: any = [];
+
+  constructor(private orderService: OrderService){}
+
+  ngOnInit(): void {
+    this.getOrder();
+  }
+  
+  public getOrder(){
+    this.orderService.getAllOrder().subscribe((result) => {
+      this.orders = result;
+      console.log(result)
+    })
+  }
 
 }
