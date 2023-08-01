7d52a0562bab7f226fbafdea08da3395869d41fd
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-08-01 13:47:40 +0100

    show details of client

--- AwidCar/src/app/client/client.component.ts
+++ AwidCar/src/app/client/client.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../service/client.service';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.scss']
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  
+  constructor(private clientService: ClientService) { }
+
+  clients: any = [];
+
+  ngOnInit(): void {
+    this.getClient();
+  }
+
+  public getClient(){
+    this.clientService.getAllClient().subscribe((result) => {
+      this.clients = result;
+      console.log(result)
+    })
+  }
 
 }
