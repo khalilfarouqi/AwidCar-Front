8fcd036118eda2788bda12e563d490c8745b9942
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-08-01 13:50:34 +0100

    get some api's in order service

--- AwidCar/src/app/service/order.service.ts
+++ AwidCar/src/app/service/order.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Order } from '../models/Order';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,16 @@ export class OrderService {
   private apiUrl = 'http://localhost:8090/v1/order';
 
   constructor(private http: HttpClient) { }
-  
-  getAllOrder() {
-    return this.http.get(`${this.apiUrl}/getAll`);
+
+  getAllOrder(): Observable<Order> {
+    return this.http.get<Order>(`${this.apiUrl}/getAll`);
+  }
+
+  getOrderById(id: number): Observable<Order> {
+    return this.http.get<Order>(`${this.apiUrl}/` + id);
+  }
+
+  deleteOrder(id: number){
+    return this.http.delete<Order>(`${this.apiUrl}/` + id);
   }
 }
