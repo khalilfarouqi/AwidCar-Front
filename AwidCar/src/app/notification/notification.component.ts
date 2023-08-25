8f7ae7695d9f4ac91590df625f6595d7e1369438
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-08-25 17:33:30 +0100

    Return Api content

--- AwidCar/src/app/notification/notification.component.ts
+++ AwidCar/src/app/notification/notification.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotifiactionService } from '../service/notifiaction.service';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss']
 })
-export class NotificationComponent {
+export class NotificationComponent implements OnInit{
+  
+  constructor(private notifiactionService: NotifiactionService) { }
+
+  notifications: any = [];
+
+  ngOnInit(): void {
+    this.getNotification();
+  }
+
+  public getNotification(){
+    this.notifiactionService.getAllNotification().subscribe((result) => {
+      this.notifications = result;
+      console.log(result)
+    })
+  }
 
 }
