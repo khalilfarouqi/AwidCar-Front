08a2ea0acac17e6204349aef6027aac56cedb44d
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-10-21 23:44:55 +0100

    Add new in car editeur component

--- AwidCar/src/app/car-editeur/car-editeur.component.ts
+++ AwidCar/src/app/car-editeur/car-editeur.component.ts
@@ -1,66 +1,90 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Brande } from '../enums/Brande.enum';
 import { Fuel } from '../enums/Fuel.enum';
 import { GearBox } from '../enums/GearBox.enum';
 import { Style } from '../enums/Style.enum';
-import { Car } from '../models/Car';
 import { CarService } from '../service/car.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ImmatriculationPopupComponent } from '../immatriculation-popup/immatriculation-popup.component';
 
 @Component({
   selector: 'app-car-editeur',
   templateUrl: './car-editeur.component.html',
   styleUrls: ['./car-editeur.component.scss']
 })
-export class CarEditeurComponent {
-
-  formData: Car = { 
-    name: '', 
-    Brande: Brande.ALFAROMEO, 
-    Matricule: '', 
-    Mileage: 0, 
-    Model: 0, 
-    HorsPower: 0, 
-    Consumption: 0, 
-    fuel: Fuel.DIESEL,
-    Bluetooth:false,
-    AirBag: false,
-    gearBox: GearBox.AUTOMATIC,
-    Seat: 0,
-    Door: 0,
-    Style: Style.COUPE,
-    bootSize: 0,
-    Price: 0,
-    Rate: 0,
-    CDM: false,
-    ProtectionVol: false,
-    RespoCivile: false,
-    BrisGlacePneus: false,
-    photo: 0 };
+export class CarEditeurComponent implements OnInit {
   
-    carList: any = [];
-  constructor(private carService: CarService,private router: Router, private route:ActivatedRoute) {}
-
-  onFormSubmit() {
-    this.carService.addCar(this.formData).subscribe(
-      response => {
-        console.log('Form submitted successfully:', response);
-      },
-      error => {
-        console.error('Error submitting form:', error);
-      }
-    );
+  formData: FormGroup;
+  
+  constructor(private fb: FormBuilder, private carService: CarService, private router: Router, public dialog: MatDialog) {
+    this.formData = this.fb.group({
+      name: ['', Validators.required],
+      brande: ['', Validators.required],
+      mileage: [0, Validators.required],
+      model: [0, Validators.required],
+      horsPower: [0, Validators.required],
+      consumption: [0, Validators.required],
+      fuel: ['', Validators.required],
+      bluetooth: [false, Validators.required],
+      airBag: [false, Validators.required],
+      gearBox: ['', Validators.required],
+      seat: [0, Validators.required],
+      door: [0, Validators.required],
+      style: ['', Validators.required],
+      bootSize: [0, Validators.required],
+      price: [0, Validators.required],
+      rate: [0, Validators.required],
+      cdm: [false, Validators.required],
+      protectionVol: [false, Validators.required],
+      respoCivile: [false, Validators.required],
+      brisGlacePneus: [false, Validators.required]
+    });
   }
-
+  
+  ngOnInit() {
+    this.formData = this.fb.group({
+      name: ['', Validators.required],
+      brande: ['', Validators.required],
+      mileage: [0, Validators.required],
+      model: [0, Validators.required],
+      horsPower: [0, Validators.required],
+      consumption: [0, Validators.required],
+      fuel: ['', Validators.required],
+      bluetooth: [false, Validators.required],
+      airBag: [false, Validators.required],
+      gearBox: ['', Validators.required],
+      seat: [0, Validators.required],
+      door: [0, Validators.required],
+      style: ['', Validators.required],
+      bootSize: [0, Validators.required],
+      price: [0, Validators.required],
+      rate: [0, Validators.required],
+      cdm: [false, Validators.required],
+      protectionVol: [false, Validators.required],
+      respoCivile: [false, Validators.required],
+      brisGlacePneus: [false, Validators.required]
+    });
+  }
+  
   MethodePost(){
-    this.carService.postData(this.formData).subscribe((item) => {
-      console.log('salam')
-      console.log(this.formData)
-      this.carList = [item,...this.carList]
-      this.router.navigate(["Admin/Car"])
+    this.carService.postData(this.formData.value).subscribe((item) => {
+      this.router.navigate(["/car"])
     })
   }
+  
+  openImmatriculationPopup(): void {
+    const dialogRef = this.dialog.open(ImmatriculationPopupComponent, {
+      width: '250px',
+      data: {}
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The immatriculation was submitted:', result);
+    });
+ }
+  
   brandes = Object.values(Brande);
   fuels = Object.values(Fuel);
   gearboxs = Object.values(GearBox);
