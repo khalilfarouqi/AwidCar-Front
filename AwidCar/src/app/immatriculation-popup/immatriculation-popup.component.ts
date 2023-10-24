754014fc9127b49866efac44cfe1af1d28293c9f
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-10-24 21:50:02 +0100

    Add new post immatriculation

--- AwidCar/src/app/immatriculation-popup/immatriculation-popup.component.ts
+++ AwidCar/src/app/immatriculation-popup/immatriculation-popup.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Series } from '../enums/Series.enum';
+import { ImmatriculationService } from '../service/immatriculation.service';
 
 @Component({
   selector: 'app-immatriculation-popup',
@@ -13,15 +14,36 @@ export class ImmatriculationPopupComponent {
     immatriculation: new FormControl('')
   });
 
-  constructor(public dialogRef: MatDialogRef<ImmatriculationPopupComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(private fb: FormBuilder, 
+    public dialogRef: MatDialogRef<ImmatriculationPopupComponent>, 
+    @Inject(MAT_DIALOG_DATA) public data: any, 
+    private immatriculationService: ImmatriculationService) {
+    this.formData = this.fb.group({
+      carNumber: [0, Validators.required],
+      series: ['', Validators.required],
+      prefectureRef: [0, Validators.required]
+    });
+  }
  
+  formData: FormGroup;
+
   onNoClick(): void {
     this.dialogRef.close();
+  }
+  
+  MethodePost(){
+    const selectedSeries = this.formData.value.series;
+    const convertedSeries = this.immatriculationService.convertArabicToLatin(selectedSeries);
+    this.immatriculationService.postData({
+      carNumber: this.formData.value.carNumber,
+      series: convertedSeries,
+      prefectureRef: {
+        id: this.formData.value.prefectureRef
+      }
+    }).subscribe();
   }
 
   disableSelect = new FormControl(false);
 
   series = Object.values(Series);
-
-
 }
