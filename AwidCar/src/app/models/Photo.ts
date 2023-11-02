976f77d47ce84816f26638ab6676ec3c961ad7a6
khalil farouqi <khalilfarouqi@MacBook-Pro-de-khalil.local>
2023-11-02 16:19:55 +0100

    reform photo module

--- AwidCar/src/app/models/Photo.ts
+++ AwidCar/src/app/models/Photo.ts
@@ -1,7 +1,7 @@
 export interface Photo {
     id?: number;
-	photo1: string;
-	photo2: string;
-	photo3: string;
-	photo4: string;
+	photo1: string | File;
+	photo2: string | File;
+	photo3: string | File;
+	photo4: string | File;
 }
