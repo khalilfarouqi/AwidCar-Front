dde6ae9ab6acb8cb563604d9e80930a703a10837
khalil farouqi <khalilfarouqi@MacBook-Pro-de-khalil.local>
2023-11-03 16:40:36 +0100

    show search content in navbar

--- AwidCar/src/app/navbar/navbar.component.ts
+++ AwidCar/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavbarService } from '../service/navbar.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  searchs: any = [];
+  label: any;
+
+  constructor(private navbarService: NavbarService, private route: ActivatedRoute) {}
+
+  public getSearch(label :string){
+    this.navbarService.getSearch(label).subscribe((result) => {
+      this.searchs = result;
+    })
+  }
 
 }
