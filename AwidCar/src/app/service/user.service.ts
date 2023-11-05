686e6e6b906a385d5c761f68c0e94b2525729b5e
khalil farouqi <khalilfarouqi@MBP-de-khalil.Home>
2023-11-05 20:07:09 +0100

    Add user service

--- AwidCar/src/app/service/user.service.ts
+++ AwidCar/src/app/service/user.service.ts
@@ -15,4 +15,16 @@ export class UserService {
   getAllUsers(): Observable<User> {
     return this.http.get<User>(`${this.apiUrl}/getAll`);
   }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/` + 1);
+  }
+
+  existsPassWord(id: number, currentPassword: string): Observable<boolean> {
+    return this.http.get<boolean>(`${this.apiUrl}/existPassword/` + id + `/` + currentPassword);
+  }
+
+  postData(data: User): Observable<User> {
+    return this.http.put<User>(this.apiUrl, data);
+  }
 }
